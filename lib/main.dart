70c3f89b9e1c1888f1948303a5bf51c9046284ae
Andrew Safwat <andrew.realme90@gmail.com>
2025-03-19 22:46:25 +0200

    start

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GitWeather2());
+}
+
+class GitWeather2 extends StatelessWidget {
+  const GitWeather2({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
