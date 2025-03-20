6a50efa9a9f181317c25fd317bfddaa9d3944379
Andrew Safwat <andrew.realme90@gmail.com>
2025-03-20 19:25:25 +0200

    to vid 25

--- lib/views/home-view.dart
+++ lib/views/home-view.dart
@@ -3,9 +3,14 @@ import 'package:gitweather2/views/search-view.dart';
 import 'package:gitweather2/widgets/no-weather-body.dart';
 import 'package:gitweather2/widgets/weather-info-body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
